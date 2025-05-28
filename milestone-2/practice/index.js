78dde6bede37cdff8acc411c70bc8a7ffc130012
dev_ismail <ismailfsbb@gmail.com>
2025-05-28 22:44:04 +0600

    index.js

--- milestone-2/practice/index.js
+++ milestone-2/practice/index.js
@@ -1,28 +1,28 @@
 // check a number is even or odd
-// let num=9;
-// if (num%2==0){
-//     console.log("Even");
-// } else{
-//     console.log("Odd");
-// }
+let num=9;
+if (num%2==0){
+    console.log("Even");
+} else{
+    console.log("Odd");
+}
 
 // Grade calculator using if else if
-// let marks=22;
-// if (marks>=80 && marks<=100){
-//     console.log("A+")
-// } else if (marks<80 && marks>=70){
-//     console.log("A");
-// } else if (marks<70 && marks>=60){
-//     console.log("A-");
-// } else if (marks<60 && marks>=50){
-//     console.log("B");
-// } else if (marks<50 && marks>=40){
-//     console.log("C");
-// } else if(marks>100 || marks<0){
-//     console.log("Not valid, please enter valid marks within 0-100");
-// } else{
-//     console.log("Fail");
-// }
+let marks=22;
+if (marks>=80 && marks<=100){
+    console.log("A+")
+} else if (marks<80 && marks>=70){
+    console.log("A");
+} else if (marks<70 && marks>=60){
+    console.log("A-");
+} else if (marks<60 && marks>=50){
+    console.log("B");
+} else if (marks<50 && marks>=40){
+    console.log("C");
+} else if(marks>100 || marks<0){
+    console.log("Not valid, please enter valid marks within 0-100");
+} else{
+    console.log("Fail");
+}
 
 // Age base condition
 let age=12;
