533179e406602b0a4dfd73415760fda563633e39
dev_ismail <ismailfsbb@gmail.com>
2025-05-30 03:13:35 +0600

    index.js

--- milestone-3/practice/index.js
+++ milestone-3/practice/index.js
@@ -38,3 +38,51 @@ let skills = ["HTML", "CSS", "JavaScript", "React"];
 for (let i=0;i<skills.length;i++){
     console.log(skills[i]);
 }
+
+
+let userAge = 32;
+if(userAge>=0 && userAge<=12){
+    console.log("Child");
+}else if(userAge>12 && userAge<18){
+    console.log("Teenager");
+}else if(userAge>17 && userAge<60){
+    console.log("Adult");    
+}else if(userAge>=60){
+    console.log("Senior");
+}else{
+    console.log('Input not valid');
+}
+
+// ternary operation
+// Check if name is "Ismail". If yes, print "Welcome back", else print "Guest user".
+
+let name="Ismail";
+let message = name==="Ismail"?"Welcome back": "Guest user";
+console.log(message);
+
+// Print numbers 1 to 20
+// Stop when the number is 10 (break)
+
+for (i = 1; i<=20 ; i++){
+    if (i==10) {
+        break;
+    }
+    console.log(i);
+}
+// Print numbers 1 to 20
+// Stop when the number is 10 (break)
+// Skip 5 (continue)
+for (let i =1; i <= 20;i++) {
+    if(i=== 5) continue;
+    if (i===10) break;
+    console.log(i);
+}
+
+// Print only working days from a list of days by skipping Sunday
+const days = ["Monday", "Tuesday", "Wednesday",  "Sunday", "Thursday", "Friday", "Saturday"];
+for (let day of days){
+    if(day==="Sunday"){
+        continue;
+    }
+    console.log(day);
+}
