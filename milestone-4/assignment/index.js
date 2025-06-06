b1c9a9f5cef4995e9bb5c5c85d99a959cad7eff5
dev_ismail <ismailfsbb@gmail.com>
2025-06-06 14:22:41 +0300

    Assignment 4 all done

--- milestone-4/assignment/index.js
+++ milestone-4/assignment/index.js
@@ -13,9 +13,6 @@ function calculator(num1, num2, operator) {
     }
 }
 console.log(calculator(2, 5, "+"));
-console.log(calculator(11, 2, "-"));
-console.log(calculator(4, 4, "*"));
-console.log(calculator(15, 3, "/")); 
 
 // Write a function that returns the factorial of a number.
 function factorial(f_num) {
@@ -42,11 +39,67 @@ function isPrime(num) {
 console.log(isPrime(11));
 
 // Write a function that converts Celsius to Fahrenheit.
+function CTF(celsius) {
+    let fahrenheit = (celsius * 9/5) + 32;
+    return fahrenheit;
+}
+console.log(CTF(32));  
+
 // Build a function that reverses a string.
+function reverseString(str) {
+  let reversed = str.split("").reverse().join("");
+  return reversed;
+}
+console.log(reverseString("Ismail"));
+
 // Write a function that finds the maximum value in an array.
+function MaxValue(arr) {
+  let max = Math.max(...arr);
+  return max;
+}
+console.log(MaxValue([1, 2, 3, 4, 5]));
+
 // Create a function that returns the Fibonacci sequence up to n terms.
+function fibonacci(n) {
+    const sequence = [];
+    if (n >= 1) sequence.push(0);
+    if (n >= 2) sequence.push(1);
+
+    for (let i = 2; i < n; i++) {
+      const next = sequence[i - 1] + sequence[i - 2];
+      sequence.push(next);
+    }
+    return sequence;
+}
+console.log(fibonacci(4));
+
 // Write a function that checks if a string is a palindrome.
+function isPalindrome(str) {
+    str = str.toLowerCase();
+    const reversed = str.split('').reverse().join('');
+    if (str === reversed) {
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log(isPalindrome("Madam"));
+
 // Implement a function that counts the number of vowels in a string.
+function countVowels(str) {
+    let count = 0;
+    str = str.toLowerCase();
+
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i];
+        if (char === "a" || char === "e" || char === "i" || char === "o" || char === "u") {
+            count++;
+        }
+    }
+    return count;
+}
+console.log(countVowels("Ismail"));
+
 // Write a function that takes a number and returns whether it’s even or odd.
 function checkEvenOrOdd(num) {
     if (num % 2 === 0) {
@@ -58,5 +111,8 @@ function checkEvenOrOdd(num) {
 console.log(checkEvenOrOdd(4));
 console.log(checkEvenOrOdd(7));
 
-
 // Create a function that calculates the area of a rectangle given width and height.
+function calculateArea(width, height) {
+    return width * height; 
+}
+console.log(calculateArea(3, 2));
